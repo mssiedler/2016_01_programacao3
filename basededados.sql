18549d02b3940d4ca27566392e52aa4b37a7500f
marcelo siedler <siedler@gmail.com>
2016-04-26 07:48:01 -0300

    Base de Dados prova

---
+++ basededados.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE apoiador
+(
+  nome character varying,
+  cnpj character varying,
+  idade int,
+  id_apoio serial NOT NULL
+);
+
+CREATE TABLE despesas
+(
+  descricao character varying,
+  valor double,
+  localdogasto character varying,
+  id_despesas serial NOT NULL
+);
+
+CREATE TABLE pergunta
+(
+  titulo character varying,
+  dica character varying,
+  niveldificuldade int,
+  id_etapa serial NOT NULL
+);
+
